7ed091c9d2cddf393bb20873750f19ff72efd075
kite <parkite576@gmail.com>
2025-10-02 17:29:14 +0900

    add brats results

--- run_eda.py
+++ run_eda.py
@@ -4,8 +4,8 @@ import subprocess
 # 처리할 상위 폴더와 출력 폴더
 folders = [
     # "sample/lucchi",
-    "sample/nuclei",
-    # "sample/brats",
+    # "sample/nuclei",
+    "sample/brats",
     # "sample/covid",
     # "sample/synthrad"
 ]
@@ -16,25 +16,30 @@ output_base = "outputs"
 img_exts = (".png", ".tif", ".jpg", ".jpeg")
 
 for folder in folders:
-    for root, _, files in os.walk(folder):
-        for f in files:
-            if f.lower().endswith(img_exts):
-                input_path = os.path.join(root, f)
-                
-                # 상대 경로에서 sample/ 뒤 경로 추출
-                rel_path = os.path.relpath(input_path, "sample")
-                # 확장자 제거
-                rel_path_noext = os.path.splitext(rel_path)[0]
-                output_path = os.path.join(output_base, rel_path_noext)
+    # 하위 폴더 리스트 가져오기
+    subfolders = [os.path.join(folder, d) for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
+    subfolders = sorted(subfolders)[:10]  # 앞에서 10개만 선택
 
-                # 폴더 자동 생성
-                os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    for subfolder in subfolders:
+        for root, _, files in os.walk(subfolder):
+            for f in files:
+                if f.lower().endswith(img_exts):
+                    input_path = os.path.join(root, f)
+                    
+                    # 상대 경로에서 sample/ 뒤 경로 추출
+                    rel_path = os.path.relpath(input_path, "sample")
+                    # 확장자 제거
+                    rel_path_noext = os.path.splitext(rel_path)[0]
+                    output_path = os.path.join(output_base, rel_path_noext)
 
-                # eda.py 실행
-                cmd = [
-                    "python", "eda.py",
-                    f"image.image.image_path={input_path}",
-                    f"image.image.output_path={output_path}"
-                ]
-                print("Running:", " ".join(cmd))
-                subprocess.run(cmd)
+                    # 폴더 자동 생성
+                    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+                    # eda.py 실행
+                    cmd = [
+                        "python", "eda.py",
+                        f"image.image.image_path={input_path}",
+                        f"image.image.output_path={output_path}"
+                    ]
+                    print("Running:", " ".join(cmd))
+                    subprocess.run(cmd)
